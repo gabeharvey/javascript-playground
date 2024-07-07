06000b7265e584e1e91f62b8eff7806b59166777
Gabe Harvey <harvey7@gmail.com>
2024-07-07 11:31:18 -0500

    Scoping test done in script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ let pets = ['Blackberry', 'Frost', 'Henrietta'];
 for(let i=0; i < pets.length; i++) {
     console.log(pets[i] + ' is my pet!')
 }
+
+function helloWorld () {
+    let pets = 'Jellybean';
+    console.log('We miss ' + pets);
+    return;
+}
+
+helloWorld ();
